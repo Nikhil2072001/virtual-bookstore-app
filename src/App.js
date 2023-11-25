764a466de679e30bf320a70ecaaeb1e47bba1267
Nikhil77sutara <nikhilchoudhary@sutara.org>
2023-11-25 21:53:48 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,82 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Navbar from './components/Navbar'
 import './App.css'
 const App = () => {
+  const[data,setData] = useState([]);
+  const[openbook,setOpenbook] = useState();
+useEffect(()=>{
+const getData = async()=>{
+  try {
+    const res = await fetch('https://www.googleapis.com/books/v1/volumes?q=harry+potter');
+    const dummydata = await res.json();
+    const res2 = await fetch('https://www.googleapis.com/books/v1/volumes?q=Sherlock+Holmes');
+    const dummydata2 = await res2.json();
+    const combinedArray = [...dummydata.items, ...dummydata2.items];
+    setData(combinedArray);
+    // setData(dummydata.items);
+    console.log(combinedArray);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData()
+console.log(openbook);
+},[openbook])
+// useEffect(()=>{
+//   const getData = async()=>{
+//     try {
+//       const res = await fetch('https://www.googleapis.com/books/v1/volumes?q=Sherlock+Holmes');
+//       const dummydata = await res.json();
+//       const combinedArray = [...data, ...dummydata.items];
+//       setData(combinedArray);
+//       console.log(dummydata);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+//   getData()
+//   },[data])
+const openBookPage = (bookIndex) => {
+  const book = data[bookIndex];
+  if (book && book.volumeInfo && book.volumeInfo.infoLink) {
+    window.open(book.volumeInfo.infoLink, '_blank'); // Open book's original page in a new tab
+  }
+};
   return (
-    <div>
+    <div className='app-main-div'>
       <Navbar/>
+      { openbook && 
+      <div className='open-book-div'>
+       <img src={data[openbook].volumeInfo.imageLinks.smallThumbnail}/>
+       <div className='book-left-part'>
+        <div className='book-left-part1'>
+          <h2>{data[openbook].volumeInfo.title}</h2>
+          <p>{`published On: ${data[openbook].volumeInfo.publishedDate}`}</p>
+        </div>
+        <p>{data[openbook].volumeInfo.description.substring(0,250)}</p>
+        <div className='rating-part'>
+          <p><b>{`Avg Rating: 4.5`}</b></p>
+          <p><b>{`Rating Count: 85`}</b></p>
+          <p><b>{`Publisher: ${data[openbook].volumeInfo.publisher}`}</b></p>
+          <p><b>{`language: ${data[openbook].volumeInfo.language}`}</b></p>
+          </div>
+          <div className='buttons-for-book'>
+          <button onClick={() => openBookPage(openbook)}>Now Read!</button>
+            <button onClick={() => openBookPage(openbook)}>More Info!</button>
+          </div>
+      </div>
+      </div>
+}
+      <div className='main-books-image'>
+          { data.length>0 && 
+            data.map((book, index) => (
+              <div key={index} onClick={()=>setOpenbook(index)}>
+                {/* <p>{book.volumeInfo.title}</p> */}
+                <img src={book.volumeInfo.imageLinks.smallThumbnail} />
+              </div>
+            ))
+          }
+      </div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,26 @@
 import React from 'react'
 // import headerIcon from './images/headerIcon.png'
-import './images/headerIcon.png'
+import  headerIcon from '../images/headerIcon.png'
+import bookname from '../images/KeazoNBOOKS.png'
+import { PiAddressBookFill } from "react-icons/pi";
+import { FaRegBell } from "react-icons/fa";
+import { SlDiamond } from "react-icons/sl";
+import profile from '../images/profileImage.png'
+import './Navbar.css'
 const Navbar = () => {
   return (
-    <div>
-        <div className='left-header'>
-            {/* <img src={headerIcon}/> */}
-        </div>
+    <div className='header-main'>
+    
+            <img src={headerIcon}/>
+            <img src={bookname}/>
+
+            <input placeholder='Search for the books you want and read it now...Sherlock Homes,Harry Potter' />
+            <button>Search</button>
+       
+        <PiAddressBookFill className='icon'/>
+        <FaRegBell className='icon'/>
+        <SlDiamond className='icon'/>
+        <img  src={profile}/>
     </div>
   )
 }
