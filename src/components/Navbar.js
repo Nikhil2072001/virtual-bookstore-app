0c6640f1e24df6d77626c853912e429a63b0f8a7
Nikhil77sutara <nikhilchoudhary@sutara.org>
2023-11-25 14:03:28 +0530

    Initialize project using Create React App

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import headerIcon from './images/headerIcon.png'
+import './images/headerIcon.png'
+const Navbar = () => {
+  return (
+    <div>
+        <div className='left-header'>
+            {/* <img src={headerIcon}/> */}
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
